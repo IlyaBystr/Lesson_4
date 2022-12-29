e4b373dbd6edc616b608fe8a2152be2c4c47a037
Ilya <fox367213@mail.com>
2022-12-29 14:24:22 +0300

    end work

--- HM_4.py
+++ HM_4.py
@@ -6,6 +6,7 @@
 # x = int(input("Введите число цифр после запятой: "))
 # print(f'число Пи равно {round(pi, x)}')
 
+
 # 2.Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
 # x = int(input("Введите число: "))
@@ -20,3 +21,36 @@
 #     else:
 #         i += 1
 # print(f"Простые множители числа {old} приведены в списке: {lst}")
+
+
+# 3.Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+# lst = list(map(int, input("Введите числа через пробел:\n").split()))
+# print(f"Исходный список: {lst}")
+# new_lst = []
+# [new_lst.append(i) for i in lst if i not in new_lst]
+# print(f"Список из неповторяющихся элементов: {new_lst}")
+
+# 4.Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+# 5.Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+
+with open('poly_1.txt', 'w', encoding='utf-8') as file:
+    file.write('2*x^2 + 5*x^5')
+with open('poly_2.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^4 + 9*x^6')
+
+with open('poly_1.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+
+
+with open('poly_2.txt','r') as file:
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+
+with open('sum_poly.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
